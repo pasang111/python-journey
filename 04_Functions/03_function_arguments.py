98720bf1e8df274a391c95fbbb56796f49f5d891
Pasang Lama <snowpasang@gmail.com>
2026-06-09 20:48:09 +0545

    basic of  multiple argument

--- 04_Functions/03_function_arguments.py
+++ 04_Functions/03_function_arguments.py
@@ -3,7 +3,7 @@
 # what are function arguments?
 # function arguments are the values we pass into a function when calling it
 
-# parameter = parameter are the  variable inside the function
+# parameter = parameter are the variable inside the function
 # argument = argument are the actual value passed while calling the function
 
 
@@ -21,3 +21,32 @@ greet("John")
 # Python matches the value based on position
 # name = "Ace"
 # name = "John"
+
+# so when we write greet("Ace")
+# Python takes the value "Ace" and stores it in the parameter called name
+# then print() displays Hello Ace
+
+
+# multiple arguments
+
+def student(name, age):
+    print(name, age)
+
+student("Ace", 20)
+student("Snow", 21)
+
+# first value goes into name and second value goes into age
+# for student("Ace", 20)
+# name = "Ace"
+# age = 20
+
+# for student("Snow", 21)
+# name = "Snow"
+# age = 21
+
+# Python always matches the values from left to right
+# first argument goes to first parameter
+# second argument goes to second parameter
+
+# if we pass too few or too many arguments
+# Python will show an error because the number of values does not match the parameters
