e810813d0e8f98ea827a37fa52321e0cdb9d76f6
Pasang Lama <snowpasang@gmail.com>
2026-06-09 20:44:14 +0545

    about fucntion and argument in py

---
+++ 04_Functions/03_function_arguments.py
@@ -0,0 +1,23 @@
+# Function Arguments
+
+# what are function arguments?
+# function arguments are the values we pass into a function when calling it
+
+# parameter = parameter are the  variable inside the function
+# argument = argument are the actual value passed while calling the function
+
+
+# positional arguments
+
+def greet(name):
+    print("Hello", name)
+
+# here "Ace" is the argument
+greet("Ace")
+
+# here "John" is another argument
+greet("John")
+
+# Python matches the value based on position
+# name = "Ace"
+# name = "John"
