ccccd3afe5d5bc46e08cd0081ea79140177368d9
Pasang Lama <snowpasang@gmail.com>
2026-06-16 17:56:08 +0800

    created simple multiplication table program

---
+++ 03_Loops/simple_multiplicationtable.py
@@ -0,0 +1,22 @@
+# Simple multiplication table
+
+number = 5
+
+for i in range(1, 11):
+    print(number, "x", i, "=", number * i)
+
+# prints the multiplication table of 5
+
+
+# Finding total using a loop
+
+numbers = [10, 20, 30, 40]
+
+total = 0
+
+for number in numbers:
+    total += number
+
+print(total)
+
+# total = 100
