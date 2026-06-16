1dbdf607d0ba7f39d449b000c9f7270b790daa5f
Pasang Lama <snowpasang@gmail.com>
2026-06-16 18:02:05 +0800

    added multiplication table of 7 example

--- 03_Loops/simple_multiplicationtable.py
+++ 03_Loops/simple_multiplicationtable.py
@@ -31,3 +31,18 @@ for i in range(1, 11):
     print(number, "x", i, "=", number * i)
 
 # prints the multiplication table of 6
+# Multiplication table of 7
+
+number = 7
+
+# range(1, 11) generates numbers from 1 to 10.
+# On each iteration, the current value is stored in i.
+# We then multiply number by i and print the result.
+
+for i in range(1, 11):
+    print(number, "x", i, "=", number * i)
+
+# Example:
+# When i = 1 -> 7 x 1 = 7
+# When i = 2 -> 7 x 2 = 14
+# This continues until i reaches 10.
