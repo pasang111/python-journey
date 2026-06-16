60c914cdc2195039850991ae3620dd5ad6872052
Pasang Lama <snowpasang@gmail.com>
2026-06-16 17:59:19 +0800

    added multiplication table examples and loop total calculator

--- 03_Loops/simple_multiplicationtable.py
+++ 03_Loops/simple_multiplicationtable.py
@@ -20,3 +20,14 @@ for number in numbers:
 print(total)
 
 # total = 100
+
+# We created the multiplication table for 5 above.
+# The same logic can be reused for other numbers by changing the value of number.
+# Here we are generating the multiplication table of 6.
+
+number = 6
+
+for i in range(1, 11):
+    print(number, "x", i, "=", number * i)
+
+# prints the multiplication table of 6
