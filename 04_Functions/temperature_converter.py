e8820b48dfc040d819d422e9c41e49b2514d4be7
Pasang Lama <snowpasang@gmail.com>
2026-06-21 22:01:51 +0800

    added temperature converter function

---
+++ 04_Functions/temperature_converter.py
@@ -0,0 +1,9 @@
+#converting celcius to fahrenheit 
+
+def convert_temperature(celsius):
+    return (celsius + 9/5) + 32
+
+celsius = float(input("Enter temperature in Celsius: "))
+
+fahrenheit = convert_temperature(celsius)
+print("Temperature in fahrenheit:", fahrenheit)
