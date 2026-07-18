b57cd59e57e09449f75bd7b9e3bd7c8c9ae81f8b
Pasang Lama <165545784+pasang111@users.noreply.github.com>
2026-07-18 23:01:57 +0800

    Add product prices printing from dictionary
    Prints the prices of products from a dictionary.

---
+++ 06_Dictionaries/dicloop.py
@@ -0,0 +1,15 @@
+products = {
+    'Laptop': 990,
+    'Smartphone': 600,
+    'Tablet': 250,
+    'Headphones': 70,
+}
+
+for price in products.values():
+    print(price)
+
+#the outpyut is this
+# 990
+# 600
+# 250
+# 70
