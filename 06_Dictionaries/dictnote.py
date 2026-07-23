dbf8bc4c0b45bdd2926f852e715072672d3bfd90
Pasang Lama <165545784+pasang111@users.noreply.github.com>
2026-07-23 22:34:13 +0800

    Add dictnote.py to create and enumerate products

---
+++ 06_Dictionaries/dictnote.py
@@ -0,0 +1,16 @@
+# creating dictionaries
+products = {
+    'Laptop': 990,
+    'Smartphone': 600,
+    'Tablet': 250,
+    'Headphones': 70,
+}
+
+#using enumerate 
+for product in enumerate(products):
+    print(product)
+# (0, 'Laptop')
+# (1, 'Smartphone')
+# (2, 'Tablet')
+# (3, 'Headphones')
+  
