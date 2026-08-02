361dfe97c70543e5827b2bbf2f596a9e1499cf53
Pasang Lama <snowpasang@gmail.com>
2026-08-02 23:29:50 +0800

    difference between dictonary and set

--- 06_Dictionaries/set.py
+++ 06_Dictionaries/set.py
@@ -5,3 +5,20 @@
 #sets are mutable and unordered, which mean that their elements are not stored in any specific order, so you cannot use indices or key to access them. They can only contain values of immutable data types like numbers, strings, and tuples. and they support mathematical set operations, including union, intersection, difference, and symmetric difference.
 
 #To define a set, we need to write its elements with in curly braces {} and separate them with commas.
+
+#For the example:
+
+my_set = {1,2,3,4,5,}
+
+#also if we ever need to define an empty set, we must use the set() function. but if we write empty curly braces {} python will automatically create an dictionary.
+
+
+empty_set = set()   # Creates an empty set
+empty_dict = {}     # Creates an empty dictionary
+
+set() #set
+{} #dictionary...
+
+# Difference:
+# A set stores only unique values.
+# A dictionary stores key-value pairs, where each key maps to a corresponding value.
