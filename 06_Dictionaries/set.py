7c12f8de89a022ecb2b6ae1542c48ea2ad3d464e
Pasang Lama <snowpasang@gmail.com>
2026-08-02 23:22:57 +0800

    added note for set..

---
+++ 06_Dictionaries/set.py
@@ -0,0 +1,7 @@
+                                            # Set in Python
+#set are one of the Python's built in data structure. One of the core characteristics of sets is that they don't store duplicate value.
+#if we try to add a duplicate value in the set, only one of the value will be stored in it.
+
+#sets are mutable and unordered, which mean that their elements are not stored in any specific order, so you cannot use indices or key to access them. They can only contain values of immutable data types like numbers, strings, and tuples. and they support mathematical set operations, including union, intersection, difference, and symmetric difference.
+
+#To define a set, we need to write its elements with in curly braces {} and separate them with commas.
