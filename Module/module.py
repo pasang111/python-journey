f2a4cf30bb52a6e7a2e70808926973304f666d51
Pasang Lama <snowpasang@gmail.com>
2026-08-09 22:57:09 +0800

    starting of module in py

---
+++ Module/module.py
@@ -0,0 +1,14 @@
+                                                                # Module in Py
+# in software development, a library is like a tool box for a developers.
+# instead of having to implement single part of the code from the scratch ourself, a library gives you pre-written and reusable code, like functions, classes and data structure that we can use in our projects.
+
+# Python has an extensive standard library with many different built-in modules.
+# Some examples of popular built-in modules are math, random, re (short for "regular expressions"), and datetime
+
+# The math module has helpful functions for performing more complex mathematical operations.
+
+# The random module is helpful for generating random numbers.
+
+# The re module is used for working with regular expressions.
+
+# And the datetime module is helpful for working with dates and times in Python.
