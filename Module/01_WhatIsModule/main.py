01cbea2589c4ca82db88b3ad6fd882811c34a1cd
Pasang Lama <snowpasang@gmail.com>
2026-09-02 23:18:09 +0800

    practice importing functions with aliases

--- Module/01_WhatIsModule/main.py
+++ Module/01_WhatIsModule/main.py
@@ -4,3 +4,10 @@ from calculator import add as addition, subtract as minus
 # Use the imported functions
 print(addition(10, 5))
 print(minus(10, 5))
+
+# Import functions with shorter aliases
+from message import welcome as start, study as learn
+
+# Call the functions using their aliases
+start()
+learn()
---
+++ Module/01_WhatIsModule/message.py
@@ -0,0 +1,7 @@
+# Display a welcome message
+def welcome():
+    print("Welcome to Python!")
+
+# Display a study message
+def study():
+    print("Keep learning every day!")
