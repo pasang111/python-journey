5464d5692d69a9144758734c844acf8a73107417
Pasang Lama <snowpasang@gmail.com>
2026-09-05 14:22:15 +0800

    Learn Python module and function imports

--- Module/01_WhatIsModule/main.py
+++ Module/01_WhatIsModule/main.py
@@ -1,7 +1,19 @@
 # import = imports the module (file), so we can use the things inside it.
 # from = imports a specific function, variable, or class from the module.
 import hello as h
+
+import math as m
+
+from math import pow as p
+from math import sqrt as s
 from calculator import add,multiply
+
 h.introduce()
+
 print(add(10,5))
+
 print(multiply(11,5))
+
+print(p(5,2))
+
+print(s(36))
