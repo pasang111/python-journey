00653c98601cf0ef1cc1810e357dd417e642780b
Pasang Lama <snowpasang@gmail.com>
2026-09-22 13:28:39 +0800

    completed musical_instrument_inventory

--- class_object/musicalinstrumentinventory.py
+++ class_object/musicalinstrumentinventory.py
@@ -1,17 +1,23 @@
 class MusicalInstrument:
-    def __init__(self, name, instrument_type):
+    def __init__(self,name,instrument_type):
         self.name = name
         self.instrument_type = instrument_type
 
     def play(self):
-        print(f'The {self.name} is fun to play!')
+        print(f'The {self.name} is fun to play')
 
     def get_fact(self):
-        return f'The {self.name} is part of the {self.instrument_type} family of instruments.'
+        return f'The {self.name} is part of {self.instrument_type} family of instruments.'
+instrument_1 = MusicalInstrument("piano","vagan")
+instrument_2 = MusicalInstrument("Violin","nyano")
 
-instrument_1 = MusicalInstrument('Oboe', 'woodwind')
-instrument_2 = MusicalInstrument('Trumpet', 'brass')
+(instrument_1.name)
+(instrument_1.instrument_type)
+(instrument_2.name)
+(instrument_2.instrument_type)
 
 instrument_1.play()
 print(instrument_1.get_fact())
 
+instrument_2.play()
+print(instrument_2.get_fact())
